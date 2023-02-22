3c4c2dc15b0a4c0d072297fdf8bce6c276cf01b8
rogerneb <roger.neb@gmail.com>
2023-02-22 12:09:16 +0100

    minor changes again

--- js/card-library.js
+++ js/card-library.js
@@ -1,91 +1,93 @@
 
 function search_card() {
   var card_search = $('#card_search').val();
-  $.getJSON("https://api.scryfall.com/cards/named?fuzzy="+card_search, function(data) {
-    console.clear()  
-    console.log(data);
+  if (card_search != "") { //something is written...
+    $.getJSON("https://api.scryfall.com/cards/named?fuzzy="+card_search, function(data) {
+      console.clear()  
+      console.log(data);
 
-    //simplify data
-    var card_img = data["image_uris"]["png"]; //img
-    var card_name = data["name"]+" "; //name
-    //symbols change
-    var card_cost = symbols_change(data["mana_cost"]);
-    var card_type = data["type_line"] //type
-    var card_text = symbols_change(data["oracle_text"]); //text
-    var card_power_toughness = power_toughness_normalizer(data["power"], data["toughness"]); //power and toughness
-    var card_price = price_normalizer(data["prices"]["eur"], data["prices"]["usd"]); //price
-    var card_link = data["scryfall_uri"]; //scryfall URL
-    //legalities
-    var card_legality_standard = get_legality(data["legalities"]["standard"]);
-    var card_legality_pioneer = get_legality(data["legalities"]["pioneer"]);
-    var card_legality_modern = get_legality(data["legalities"]["modern"]);
-    var card_legality_legacy = get_legality(data["legalities"]["legacy"]);
-    var card_legality_vintage = get_legality(data["legalities"]["vintage"]);
-    var card_legality_commander = get_legality(data["legalities"]["commander"]);
-    var card_legality_alchemy = get_legality(data["legalities"]["alchemy"]);
-    var card_legality_explorer = get_legality(data["legalities"]["explorer"]);
-    var card_legality_brawl = get_legality(data["legalities"]["brawl"]);
-    var card_legality_historic = get_legality(data["legalities"]["historic"]);
-    var card_legality_pauper = get_legality(data["legalities"]["pauper"]);
-    var card_legality_penny = get_legality(data["legalities"]["penny"]);
+      //simplify data
+      var card_img = data["image_uris"]["png"]; //img
+      var card_name = data["name"]+" "; //name
+      //symbols change
+      var card_cost = symbols_change(data["mana_cost"]);
+      var card_type = data["type_line"] //type
+      var card_text = symbols_change(data["oracle_text"]); //text
+      var card_power_toughness = power_toughness_normalizer(data["power"], data["toughness"]); //power and toughness
+      var card_price = price_normalizer(data["prices"]["eur"], data["prices"]["usd"]); //price
+      var card_link = data["scryfall_uri"]; //scryfall URL
+      //legalities
+      var card_legality_standard = get_legality(data["legalities"]["standard"]);
+      var card_legality_pioneer = get_legality(data["legalities"]["pioneer"]);
+      var card_legality_modern = get_legality(data["legalities"]["modern"]);
+      var card_legality_legacy = get_legality(data["legalities"]["legacy"]);
+      var card_legality_vintage = get_legality(data["legalities"]["vintage"]);
+      var card_legality_commander = get_legality(data["legalities"]["commander"]);
+      var card_legality_alchemy = get_legality(data["legalities"]["alchemy"]);
+      var card_legality_explorer = get_legality(data["legalities"]["explorer"]);
+      var card_legality_brawl = get_legality(data["legalities"]["brawl"]);
+      var card_legality_historic = get_legality(data["legalities"]["historic"]);
+      var card_legality_pauper = get_legality(data["legalities"]["pauper"]);
+      var card_legality_penny = get_legality(data["legalities"]["penny"]);
 
-    //write data in the html
-    $("#error").html(""); //delete error
-    $('#card-img').html("<img src="+card_img+">"); //img
-    $("#card-name").html(card_name); //name
-    $("#card-cost").html(card_cost); //cost
-    $("#card-type").html(card_type); //type
-    $("#card-text").html(card_text); //text
-    $("#card-power-toughness").html(card_power_toughness); //text
-    $("#card-price").html(card_price); //card prices
-    $("#card-link").html("<a href="+card_link+" target='_blank' class='scryfall-link'>View it on Scryfall</a>");
-    $("#card-legality-standard").html("<span>Standard:</span> "+card_legality_standard);
-    $("#card-legality-pioneer").html("<span>Pioneer:</span> "+card_legality_pioneer);
-    $("#card-legality-modern").html("<span>Modern:</span> "+card_legality_modern);
-    $("#card-legality-legacy").html("<span>Legacy:</span> "+card_legality_legacy);
-    $("#card-legality-vintage").html("<span>Vintage:</span> "+card_legality_vintage);
-    $("#card-legality-commander").html("<span>Commander:</span> "+card_legality_commander);
-    $("#card-legality-alchemy").html("<span>Alchemy:</span> "+card_legality_alchemy);
-    $("#card-legality-explorer").html("<span>Explorer:</span> "+card_legality_explorer);
-    $("#card-legality-brawl").html("<span>Brawl:</span> "+card_legality_brawl);
-    $("#card-legality-historic").html("<span>Historic:</span> "+card_legality_historic);
-    $("#card-legality-pauper").html("<span>Pauper:</span> "+card_legality_pauper);
-    $("#card-legality-penny").html("<span>Penny:</span> "+card_legality_penny);
+      //write data in the html
+      $("#error").html(""); //delete error
+      $('#card-img').html("<img src="+card_img+">"); //img
+      $("#card-name").html(card_name); //name
+      $("#card-cost").html(card_cost); //cost
+      $("#card-type").html(card_type); //type
+      $("#card-text").html(card_text); //text
+      $("#card-power-toughness").html(card_power_toughness); //text
+      $("#card-price").html(card_price); //card prices
+      $("#card-link").html("<a href="+card_link+" target='_blank' class='scryfall-link'>View it on Scryfall</a>");
+      $("#card-legality-standard").html("<span>Standard:</span> "+card_legality_standard);
+      $("#card-legality-pioneer").html("<span>Pioneer:</span> "+card_legality_pioneer);
+      $("#card-legality-modern").html("<span>Modern:</span> "+card_legality_modern);
+      $("#card-legality-legacy").html("<span>Legacy:</span> "+card_legality_legacy);
+      $("#card-legality-vintage").html("<span>Vintage:</span> "+card_legality_vintage);
+      $("#card-legality-commander").html("<span>Commander:</span> "+card_legality_commander);
+      $("#card-legality-alchemy").html("<span>Alchemy:</span> "+card_legality_alchemy);
+      $("#card-legality-explorer").html("<span>Explorer:</span> "+card_legality_explorer);
+      $("#card-legality-brawl").html("<span>Brawl:</span> "+card_legality_brawl);
+      $("#card-legality-historic").html("<span>Historic:</span> "+card_legality_historic);
+      $("#card-legality-pauper").html("<span>Pauper:</span> "+card_legality_pauper);
+      $("#card-legality-penny").html("<span>Penny:</span> "+card_legality_penny);
 
-    //back to top
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+      //back to top
+      document.body.scrollTop = 0; // For Safari
+      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 
-  })
-  .fail(function() { //when the card doesnt exist
-    $("#error").html("<img src='img/sad.png' alt='sad face'><br><span id='error-ooops'><b>Oops!</b></span><br>It looks like we can't find the card you're looking for. It may not exist. Check that you typed it correctly and try again."); //error
-    //cleaning previous card info
-    $('#card-img').html(""); //img
-    $("#card-name").html(""); //name
-    $("#card-cost").html(""); //cost
-    $("#card-type").html(""); //type
-    $("#card-text").html(""); //text
-    $("#card-power-toughness").html(""); //text
-    $("#card-price").html(""); //card prices
-    $("#card-link").html(""); //scryfall button
-    //clean legalities
-    $("#card-legality-standard").html("");
-    $("#card-legality-pioneer").html("");
-    $("#card-legality-modern").html("");
-    $("#card-legality-legacy").html("");
-    $("#card-legality-vintage").html("");
-    $("#card-legality-commander").html("");
-    $("#card-legality-alchemy").html("");
-    $("#card-legality-explorer").html("");
-    $("#card-legality-brawl").html("");
-    $("#card-legality-historic").html("");
-    $("#card-legality-pauper").html("");
-    $("#card-legality-penny").html("");
+    })
+    .fail(function() { //when the card doesnt exist
+      $("#error").html("<img src='img/sad.png' alt='sad face'><br><span id='error-ooops'><b>Oops!</b></span><br>It looks like we can't find the card you're looking for. It may not exist. Check that you typed it correctly and try again."); //error
+      //cleaning previous card info
+      $('#card-img').html(""); //img
+      $("#card-name").html(""); //name
+      $("#card-cost").html(""); //cost
+      $("#card-type").html(""); //type
+      $("#card-text").html(""); //text
+      $("#card-power-toughness").html(""); //text
+      $("#card-price").html(""); //card prices
+      $("#card-link").html(""); //scryfall button
+      //clean legalities
+      $("#card-legality-standard").html("");
+      $("#card-legality-pioneer").html("");
+      $("#card-legality-modern").html("");
+      $("#card-legality-legacy").html("");
+      $("#card-legality-vintage").html("");
+      $("#card-legality-commander").html("");
+      $("#card-legality-alchemy").html("");
+      $("#card-legality-explorer").html("");
+      $("#card-legality-brawl").html("");
+      $("#card-legality-historic").html("");
+      $("#card-legality-pauper").html("");
+      $("#card-legality-penny").html("");
 
-    //back to top
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-  })
+      //back to top
+      document.body.scrollTop = 0; // For Safari
+      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    })
+  }  
 }    
   
 //SYMBOLS
